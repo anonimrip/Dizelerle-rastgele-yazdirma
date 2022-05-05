cb2a630592ecc451891742fee4ddf83c17ba2895
EREN FELEKOGLU <EREN FELEKOGLU@BIL-12>
2022-05-05 11:05:35 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] günler = {"Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"};
+            string[] yemek = { "Nargile","Pilav","Fasulley","Guru","Marul Çorbası","Ananas suyu","Mısır","Mercimek","Tavuk","Döner","Tost","Mangalda Sucuk","patates","kızartma","kadın budu" };
+            string uret = "";
+
+            Random rastgele = new Random();
+          
+            for(int i=0;i<7;i++)
+            {
+                uret = yemek[rastgele.Next(yemek.Length)];
+                Console.WriteLine(günler[i]);
+                Console.WriteLine(uret);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        
+        
+        }
+    }
+}
